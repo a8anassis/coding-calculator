5d5760a12a4345e36b7599d6310c6b842478ce55
Th. Androutsos <a8anassis@gmail.com>
2024-12-15 23:45:48 +0200

    hotfix in div. ArithmeticException added for dividing by zero

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.calc;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,7 +16,12 @@ public class Main {
         return a - b;
     }
 
-    public static int div(int a, int b) {
-        return a / b;
+    public static int div(int a, int b) throws ArithmeticException {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.err.println("Error. Divide by zero" + Arrays.toString(e.getStackTrace()));
+            throw e;
+        }
     }
 }
