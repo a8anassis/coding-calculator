e2dcccebce441ca14c647a51f19d014fb823d6ea
Th. Androutsos <a8anassis@gmail.com>
2024-12-17 15:28:57 +0200

    println with sum added

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -3,7 +3,7 @@ package gr.aueb.cf.calc;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Sum: " + add(5, 12));
     }
 
     public static int add(int a, int b) {
